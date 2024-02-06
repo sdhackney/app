d6c17157aa263e3f31786844d2a8c998e48b70a8
sdhackney <sdhackney@protonmail.com>
2024-02-06 07:02:51 -0800

    changed csv to modified vehicles us

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('modified_vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
 st.header('Vehicle Data Analysis')
